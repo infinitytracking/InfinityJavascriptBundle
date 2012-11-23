11051e8d4dcf596d9f4775873463c40be8fcc615
Chris Sedlmayr <chris@sedlmayr.co.uk>
2012-11-23 09:01:33 +0000

    Adds the bundle definition class

---
+++ InfinityTrackingInfinityJavaScriptBundle.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of the InfinityTrackingInfinityJavaScriptBundle package.
+ *
+ * (c) InfinityTracking <http://github.com/InfinityTracking/InfinityJavaScriptBundle>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace InfinityTracking\Bundle\InfinityJavaScriptBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class InfinityTrackingInfinityJavaScriptBundle extends Bundle
+{
+}
